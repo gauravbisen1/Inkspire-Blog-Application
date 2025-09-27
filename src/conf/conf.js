6d3dcf0e2f47402623cfccaee61786024f351e0e
gauravbisen1 <gouravbisen123@gmail.com>
2025-09-27 13:50:34 +0530

    Inkspire

---
+++ src/conf/conf.js
@@ -0,0 +1,15 @@
+//to get in string , to not get any error
+
+const conf = {
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+
+    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+
+    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+
+    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+
+    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+}
+
+export default conf
