ffa3216a08e9ca721ea923cb12f975fd256e3caa
gauravbisen1 <gouravbisen123@gmail.com>
2025-11-02 23:30:24 +0530

    building application

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,12 @@ import Footer from "./footer/Footer"
 import Container from "./container/Container"
 import Logo from "./Logo"
 import LogoutBtn from "./header/LogoutBtn"
+import RTE from "./RTE"
+import Signup from "./Signup"
+import Login from "./Login"
+import PostForm from "./post-form/PostForm"
+import PostCard from "./PostCard"
+import AuthLayout from "./AuthLayout"
 
 export {
     Header,
@@ -10,4 +16,10 @@ export {
     Container,
     Logo,
     LogoutBtn,
+    RTE,
+    Signup,
+    Login,
+    PostForm,
+    PostCard,
+    AuthLayout
 }
